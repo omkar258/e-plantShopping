d9b9058ba983ad43370bb370d2956fb46674a720
omkar258 <161976319+omkar258@users.noreply.github.com>
2026-04-09 15:41:01 +0530

    Add AboutUs component for Paradise Nursery

---
+++ src/AboutUs.jsx
@@ -0,0 +1,11 @@
+export default function AboutUs() {
+  return (
+    <div>
+      <h2>About Paradise Nursery</h2>
+      <p>
+        Paradise Nursery offers a wide variety of indoor plants to
+        enhance your home and workspace.
+      </p>
+    </div>
+  );
+}
